8bf60734b5f153db333006ef09a1b7015986ef46
Mark Hampton <mark.hampton@ieee.org>
2023-03-06 13:30:04 +0100

    Removed the cache because it makes less sense in conversations

--- server.js
+++ server.js
@@ -8,21 +8,36 @@ import { WebSocketServer } from 'ws'
 import https from 'https'
 import fs from 'fs'
 //  If the module is exporting a named export, use curly braces to destructure the named export. If the module is exporting a default export, import it directly without using curly braces. 
-import { TextDecoder } from 'util'
 import bodyParser from 'body-parser'
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { MongoClient, ServerApiVersion } from 'mongodb'
 import beaverLogger from 'beaver-logger/server/server.js';
+import Keyv from 'keyv'
+import KeyvBetterSqlite3 from 'keyv-better-sqlite3';
+import { encode } from 'gpt-3-encoder';
+import { v4 as uuidv4 } from'uuid'
+
+
+// Serve static files from the public directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // OPENAI_API_KEY
 // MONGODB_KEY
 dotenv.config()
 
-const use_cache = config.get('Options.use_cache');
 const use_logger = config.get('Options.use_logger');
 
-// automatically handle appending the previous messages to the prompt and attempt to optimize for the available tokens (which defaults to 4096).
+const store = new KeyvBetterSqlite3({
+  uri: 'sqlite://database.sqlite'
+})
+const messageStore = new Keyv({ store, namespace: 'chatgpt-demo' })
+
+
+// ChatGPTAPI looks after conversations for us
+// To change some properties we need to restart the connection 
+/*
 const api = new ChatGPTAPI({
   apiKey: process.env.OPENAI_API_KEY,
   completionParams: {
@@ -30,8 +45,10 @@ const api = new ChatGPTAPI({
     temperature: 0.0,
     top_p: 1.0
   },
+  messageStore,
   //debug: true
 })
+*/
 
 const serverOptions = {
   key: fs.readFileSync('key.pem'),
@@ -43,29 +60,22 @@ const app = express();
 app.use(bodyParser.json());
 
 const server = https.createServer(serverOptions, app)
-//const server = http.createServer(app)
 
 const websocketServer = new WebSocketServer({ server });
 
-const decoder = new TextDecoder('utf-8');
-
+// Sessions are stored in memory so a server restart starts new conversations
 const clients = new Map();
-
-function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
 
 //when a websocket connection is established
 websocketServer.on('connection', (ws) => {
 
   console.log("websocketServer.on 'connection'")
 
-  const sessionId = uuidv4();
+  let sessionId = uuidv4();
 
-  clients.set(ws, { "sessionId": sessionId});
+  // Here the client is tied to the web socket 
+  // If we reconnect we will lose this 
+  //clients.set(ws, { "sessionId": sessionId});
  
   //send feedback to the incoming connection
   ws.send('{ "connection" : "ok", "sessionId" : "' + sessionId + '"}');
@@ -75,82 +85,72 @@ websocketServer.on('connection', (ws) => {
 
     const j = JSON.parse(message)
     if (j?.sessionId) {
-      console.log("sessionId: ", j.sessionId)
+      //console.log("sessionId: ", j.sessionId)
+      sessionId = j.sessionId
+    } 
+    
+    if (!clients.has(sessionId)) {
+      //console.log("Creating ", sessionId)
+      clients.set(sessionId, {});
     }
-    const clientData = clients.get(ws);
+
+    const clientData = clients.get(sessionId);
 
     if (j?.prompt) {
       const { langModel, temperature, maxTokens, impersonation } = j;
       const prompt = impersonation?
                   `pretend you are ${impersonation}, ${j.prompt}`:
                   j.prompt;
-      console.log("prompt ",prompt);
+      //console.log("prompt ",prompt);
 
-      let botMsg = ""
-  
-      if (use_cache) {
-        const cacheKey = [prompt, langModel, temperature, maxTokens, impersonation].join('-').replace(/\s+/g, '-').toLowerCase();
-        console.log("cacheKey", cacheKey);
-  
-        botMsg = myCache.get(cacheKey);
-        if (botMsg){
-          console.log("value found in cache.", botMsg);
-        } else {
-          console.log("value missing in cache. fecthing open api end point...");
-        }
-      }
-  
-      if (!botMsg) {
+      const currentDate = new Date().toISOString().split('T')[0]
+      const systemMessage = `You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.\nKnowledge cutoff: 2021-09-01\nCurrent date: ${currentDate}`
+      // Also need to account for the system message and some margin because the token count may not be exact.
+      const availableTokens = (maxTokens - Math.floor(maxTokens * 0.1)) - encode(prompt).length - encode(systemMessage).length
+      let maxResponseTokens = 1000
+      maxResponseTokens = availableTokens < maxResponseTokens ? availableTokens : maxResponseTokens
+      
+      // This is a hack to get parameters into the API
+      // We should be able to change this on the fly, I requested a feature 
+      // https://github.com/transitive-bullshit/chatgpt-api/issues/434
+      // It seems the API calls _buildMessages that will not restore messages
+      // In https://github.com/transitive-bullshit/chatgpt-api/blob/main/bin/cli.js there is an example
+      // of continuing conversations 
+      const api = new ChatGPTAPI({
+        apiKey: process.env.OPENAI_API_KEY,
+        completionParams: {
+          model: langModel, //'gpt-3.5-turbo-0301',
+          temperature: temperature,
+          top_p: 1.0,
+        },
+        systemMessage: systemMessage,
+        messageStore,
+        maxResponseTokens: maxResponseTokens,
+        maxModelTokens: maxTokens,
+        //debug: true 
+      })
 
-        // This is a hack to get parameters into the API
-        // We should be able to change this on the fly, I requested a feature 
-        // https://github.com/transitive-bullshit/chatgpt-api/issues/434
-        const api = new ChatGPTAPI({
-          apiKey: process.env.OPENAI_API_KEY,
-          completionParams: {
-            model: langModel, //'gpt-3.5-turbo-0301',
-            temperature: temperature,
-            top_p: 1.0,
-          },
-          maxResponseTokens: maxTokens,
-          //debug: true
-        })
-
-        function logIncrementalOutput(partialResponse, ws) {
-          const incr = JSON.stringify(partialResponse.delta)
-          ws.send(`{"stream" : ${incr}}`)
-        }
-  
-        const response = await api.sendMessage(prompt, {
-          // print the partial response as the AI is "typing"
-          onProgress: (partialResponse) => logIncrementalOutput(partialResponse, ws),
-          parentMessageId: clientData?.parentMessageId 
-        });
-        
-        //console.log("response ",response.detail.choices)
-        clientData.parentMessageId = response.id
-        botMsg = response.text
-      }
-  
-      if (use_cache) {
-        const cacheSetSuccess = myCache.set( cacheKey, botMsg);
-        console.log("cacheSetSuccess: ", cacheSetSuccess);
-        ws.send(`{"stream" : ${botMsg}}`)
+      function logIncrementalOutput(partialResponse, ws) {
+        const incr = JSON.stringify(partialResponse.delta)
+        ws.send(`{"stream" : ${incr}}`)
       }
 
+      const response = await api.sendMessage(prompt, {
+        // print the partial response as the AI is "typing"
+        onProgress: (partialResponse) => logIncrementalOutput(partialResponse, ws),
+        parentMessageId: clientData?.parentMessageId 
+      });
+      
+      //console.log("response ", response.text, response.id)
+      clientData.parentMessageId = response.id
+  
     }
-
-    const client = clients.get(ws);
-    client.lastMessage = decoder.decode(message);
 
   });
 
   ws.on('close', () => {
     console.log('Client disconnected');
-    // Remove the WebSocket connection from the clients map
-    const client = clients.get(ws);
-    const sessionId =  client.sessionId
-    clients.delete(ws);
+    clients.delete(sessionId); 
   });
 
 });
@@ -158,9 +158,6 @@ websocketServer.on('connection', (ws) => {
 app.use(cors());
 app.use(express.json());
 
-// Serve static files from the public directory
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'public')));
 
 const myCache = new NodeCache( { 
