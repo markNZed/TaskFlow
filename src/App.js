06b4db7f89edde25e697eadde9fa05fe6cf42af9
Mark Hampton <mark.hampton@ieee.org>
2023-04-13 12:54:02 +0000

    Geolocation in separate file

--- src/App.js
+++ src/App.js
@@ -6,50 +6,11 @@ import Workflows from "./components/Workflow/Workflows"
 import { WebSocketProvider } from './contexts/WebSocketContext';
 import { GlobalStateProvider } from './contexts/GlobalStateContext'
 import { socketUrl } from './config';
+import { useGeolocation } from './useGeolocation';
 
 function App() {
-  const [location, setLocation] = useState(null);
-  const [address, setAddress] = useState('');
 
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        position => {
-          setLocation({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-          });          
-        },
-        error => {
-          console.error(error);
-        }
-      );
-    } else {
-      console.error('Geolocation is not supported by this browser.');
-    }
-  }, []);
-
-  useEffect(() => {
-    if (location) {
-      const reverse_lookup = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${location.latitude}&lon=${location.longitude}&zoom=18&addressdetails=1`
-      async function fetchData(reverse_lookup) { 
-        let result = await fetch(reverse_lookup)
-        .then((response) => response.json())
-        .catch((err) => {
-            console.log(err.message);
-        });
-        if (result?.display_name) {
-          return result.display_name
-        } else {
-          return ''
-        }
-      }
-      fetchData(reverse_lookup).then((address) => {
-        setAddress(address)
-        console.log("Address set " + address)
-      })
-    }
-  }, [location]);
+  const { address, error : locationError } = useGeolocation();
 
   return (
     <GlobalStateProvider>
