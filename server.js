35642bda3f57a88f90895eee6926f7663b78fc0d
Mark Hampton <mark.hampton@ieee.org>
2023-04-20 13:57:42 +0000

    Split chat into two functions and separated functionality into a new file

--- server.js
+++ server.js
@@ -55,7 +55,6 @@ Future
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
-import { ChatGPTAPI } from 'chatgpt'
 import { WebSocketServer } from 'ws'
 import http from 'http'
 import bodyParser from 'body-parser'
@@ -63,10 +62,10 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import Keyv from 'keyv'
 import KeyvBetterSqlite3 from 'keyv-better-sqlite3';
-import { encode } from 'gpt-3-encoder';
 import { v4 as uuidv4 } from 'uuid'
 import { tasksFn } from './tasksFn/tasksFn.mjs';
 import { utils } from './utils.mjs';
+import { chat_async, initChatGlobals } from './chat.mjs';
 import { error } from 'console'
 dotenv.config()
 
@@ -236,6 +235,20 @@ function wsSendObject(ws, message = {}) {
   }
 }
 
+const chatConfig = {
+  defaults,
+  agents,
+  messagesStore_async,
+  users,
+  cache_async,
+  DUMMY_OPENAI,
+  connections,
+  CACHE_ENABLE,
+  wsSendObject,
+}
+
+initChatGlobals(chatConfig);
+
 websocketServer.on('connection', (ws) => {
 
   console.log("websocketServer.on")
@@ -270,215 +283,6 @@ websocketServer.on('connection', (ws) => {
   });
 
 });
-
-function SendIncrementalWs(partialResponse, instanceId, ws) {
-  const incr = JSON.stringify(partialResponse.delta)
-  if (ws) {
-    if (ws.data['delta_count'] && ws.data['delta_count'] % 20 === 0) {
-      const message = {'instanceId' : instanceId, 'text' : partialResponse.text}
-      wsSendObject(ws, message)
-    } else if (incr) {
-      const message = {'instanceId' : instanceId, 'delta' : partialResponse.delta}
-      wsSendObject(ws, message)
-    }
-    ws.data['delta_count'] += 1
-    //console.log("ws.data['delta_count'] " + ws.data['delta_count'])
-  } else {
-    console.log("Lost websocket in SendIncrementalWs for instanceId " + instanceId)
-  }
-}
-
-// Move into another file
-// Split into preparing prompt, requesting to API
-async function chat_async(task) {
-
-  const sessionId = task.sessionId
-  let ws = connections.get(sessionId);
-  if (!ws) {
-    console.log("Warning: chat_async could not find ws for " + sessionId)
-  }
-  let systemMessage = ''
-  let lastMessageId = null;
-  let initializing = false;
-  let use_cache = CACHE_ENABLE
-  let server_task = false;
-
-  let langModel = task?.model || defaults.langModel
-  let temperature = task?.temperature || defaults.temperature
-  let maxTokens = task?.maxTokens || defaults.maxTokens
-
-  let prompt = task?.prompt
-  let agent = agents[task.agent]
-
-  if (task?.one_thread) {
-    // Prefix with location when it has changed
-    if (task?.new_address) {
-      prompt = "Location: " + task.address + "\n" + prompt
-    }
-    // Prefix prompt with date/time
-    const currentDate = new Date();
-    prompt = 'Time: ' + utils.formatDateAndTime(currentDate) + "\n" + prompt
-    console.log("one_thread prompt : " + prompt)
-  }
-  
-  if (typeof task.use_cache !== 'undefined') {
-    use_cache = task.use_cache
-    console.log("Task set cache " + use_cache)
-  }
-
-  if (typeof agent?.prepend_prompt !== 'undefined') {
-    prompt = agent.prepend_prompt + prompt
-    console.log("Prepend agent prompt " + agent.prepend_prompt)
-  }
-
-  if (typeof agent?.append_prompt !== 'undefined') {
-    prompt += agent.append_prompt
-    console.log("Append agent prompt " + agent.append_prompt)
-  }
-
-  if (typeof task.initialize !== 'undefined') {
-    initializing = task.initialize
-    console.log("Task initializing")
-  }
-
-  if (typeof task.server_task !== 'undefined') {
-    server_task = task.server_task
-    console.log("Task server_task")
-  }
-
-  if (!initializing) {
-    lastMessageId = await messagesStore_async.get(task.threadId + agent.id + 'parentMessageId')
-    console.log("!initializing task.threadId " + lastMessageId)
-  }
-
-  if (!lastMessageId || initializing) {
-
-    if (agent?.messages) {
-      lastMessageId = await utils.processMessages_async(agent.messages, messagesStore_async, lastMessageId)
-      console.log("Initial messages from agent " + agent.name + " " + lastMessageId)
-    }
-
-    if (task?.messages) {
-      lastMessageId = await utils.processMessages_async(task.messages, messagesStore_async, lastMessageId)
-      console.log("Messages extended from task.name " + task.name + " lastMessageId " + lastMessageId)
-    }
-  
-    if (agent?.system_message) {
-      systemMessage = agent.system_message;
-      console.log("Sytem message from agent " + agent.name)
-    }
-
-    if (users[task.userId] && task.dyad) {
-      let user = users[task.userId];
-      systemMessage += ` Vous etes en dyad avec votre user qui s'appelle ${user?.name}. ${user?.profile}`;
-      console.log("Dyad in progress between " + agent.name + " and " + user?.name)
-    }
-
-  }
-
-  // Need to account for the system message and some margin because the token count may not be exact.
-  //console.log("prompt " + prompt + " systemMessage " + systemMessage)
-  if (!prompt) {console.log("ERROR: expect prompt to calculate tokens")}
-  if (!systemMessage) {console.log("Warning: expect systemMessage to calculate tokens unless vierge")}
-  const availableTokens = (maxTokens - Math.floor(maxTokens * 0.1)) - encode(prompt).length - encode(systemMessage).length
-  let maxResponseTokens = 1000 // Leave room for conversation history
-  maxResponseTokens = availableTokens < maxResponseTokens ? availableTokens : maxResponseTokens
-  console.log("Tokens maxTokens " + maxTokens + " maxResponseTokens " + maxResponseTokens)
-  
-  // This is a hack to get parameters into the API
-  // We should be able to change this on the fly, I requested a feature 
-  // https://github.com/transitive-bullshit/chatgpt-api/issues/434
-  const api = new ChatGPTAPI({
-    apiKey: process.env.OPENAI_API_KEY,
-    completionParams: {
-      top_p: 1.0,
-    },
-    messageStore: messagesStore_async,
-    maxResponseTokens: maxResponseTokens,
-    maxModelTokens: maxTokens,
-    debug: true
-  })
-
-  const messageParams = {
-    completionParams: {
-      model: langModel,
-      temperature: temperature,
-    },
-    parentMessageId: lastMessageId,
-    systemMessage: systemMessage,
-  };
-
-  if (!server_task) {
-    messageParams['onProgress'] = (partialResponse) => SendIncrementalWs(partialResponse, task.instanceId, ws)
-  }
-
-  messagesStore_async.set(task.threadID + agent.id + 'systemMessage', messageParams.systemMessage)
-
-  let cachedValue = '';
-  let cacheKey = '';
-  if (use_cache) {
-    const messagesText = await utils.messagesText_async(messagesStore_async, lastMessageId)
-    const cacheKeyText = [
-      messageParams.systemMessage,  
-      JSON.stringify(messageParams.completionParams), 
-      prompt, 
-      messagesText
-    ].join('-').replace(/\s+/g, '-')
-    cacheKey = utils.djb2Hash(cacheKeyText);
-    console.log("cacheKey " + cacheKey)
-    cachedValue = await cache_async.get(cacheKey);
-  }
-
-  // Message can be sent from one of multiple sources
-  function message_from(source, text, server_task, ws) {
-     // Don't add ... when response is fully displayed
-    console.log("Response from " + source + " : " + text.slice(0, 20) + " ...");
-    const message = {
-      'instanceId' : task.instanceId,
-      'final' : text
-    }
-    if (ws) {
-      ws.data['delta_count'] = 0
-      if (!server_task) { wsSendObject(ws, message) }
-    } else {
-      console.log("Lost ws in message_from")
-    }
-  }
-
-  let response_text_promise = Promise.resolve("");
-
-  if (cachedValue && cachedValue !== undefined) {
-    messagesStore_async.set(task.threadID + agent.id + 'parentMessageId', cachedValue.id)
-    let text = cachedValue.text;
-    message_from('cache', text, server_task, ws)
-    response_text_promise = Promise.resolve(text);
-  } else {
-    // Need to return a promise
-    if (DUMMY_OPENAI) {
-      const text = "Dummy text"
-      message_from('Dummy API', text, server_task, ws)
-      response_text_promise = Promise.resolve(text);
-    } else {
-      response_text_promise = api.sendMessage(prompt, messageParams)
-      .then(response => {
-        messagesStore_async.set(task.threadID + agent.id + 'parentMessageId', response.id)
-        let text = response.text;
-        message_from('API', text, server_task, ws)    
-        if (use_cache) {
-          cache_async.set(cacheKey, response);
-          console.log("cache stored key ", cacheKey);
-        }
-        return text
-      })
-      .catch(error => {
-        let text = "ERROR " + error.message
-        message_from('API', text, server_task, ws)    
-        return text
-      })
-    } 
-  }
-  return response_text_promise
-}
 
 const allowedOrigins = [CLIENT_URL];
 
