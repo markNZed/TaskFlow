3516a16d8a138f2579f73a0fa2bd686a56ff6874
Mark Hampton <mark.hampton@ieee.org>
2023-06-02 15:16:26 +0000

    config cleanup

--- processor/react/src/config.js
+++ processor/react/src/config.js
@@ -4,17 +4,27 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
-import { appLabel as appLabelDefault, appName, appAbbrev, TASKHUB_URL } from "./shared/config.mjs"
+import { appLabel as appLabelDefault, appName, appAbbrev } from "./shared/config.mjs"
 
 export { appName, appAbbrev }
 export const appLabel = process.env.REACT_APP_LABEL || appLabelDefault; 
 
 const socketProtocol = window.location.protocol === "https:" ? "wss:" : "ws:";
 
-let hubUrl = TASKHUB_URL;
+let hubUrl = "http://localhost:5001/hub";
+if (window.location.hostname !== "localhost") {
+    hubUrl = `${window.location.protocol}//${window.location.hostname}/hub`
+}
 if (process.env.REACT_APP_TASKHUB_URL) {
-    hubUrl = window.location.protocol + "//" + process.env.REACT_APP_TASKHUB_URL
-};
-export { hubUrl };
+    hubUrl = `${window.location.protocol}//${process.env.REACT_APP_TASKHUB_URL}`
+}
 
-export const hubSocketUrl = process.env.REACT_APP_TASKHUB_WS || `${socketProtocol}//${window.location.hostname}/hub/ws`;
+let hubSocketUrl = "ws://localhost:5001/hub/ws"
+if (window.location.hostname !== "localhost") {
+    hubSocketUrl = `${socketProtocol}//${window.location.hostname}/hub/ws`
+}
+if (process.env.REACT_APP_TASKHUB_WS) {
+    hubSocketUrl = `${socketProtocol}//${process.env.REACT_APP_TASKHUB_WS}`
+}
+
+export { hubUrl, hubSocketUrl };
