921f61f83b7ce0b2bfa6f0e316a8ad56630c34f0
Mark Hampton <mark.hampton@ieee.org>
2023-08-26 10:53:04 +0000

    Removed 0 delay

--- processor/react/src/components/Tasks/TaskShowInstruction.js
+++ processor/react/src/components/Tasks/TaskShowInstruction.js
@@ -67,7 +67,7 @@ const TaskShowInstruction = (props) => {
   */
   useEffect(() => {
     if (!fsmState) {return}
-    setTimeout(() => {
+    //setTimeout(() => {
       // events not related to a particular state
       if (task.input.exit && fsmState.value !== 'finish') {
         fsmSend('finish');
@@ -86,7 +86,7 @@ const TaskShowInstruction = (props) => {
         default:
           console.log("FSM ERROR unknown state : " + fsmState.value);
       }
-    }, 0);
+    //}, 0);
   }, [fsmState, task]);
 
   // Each time this component is mounted reset the task state
